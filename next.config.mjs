eb1b594226a7b82c58c2018ad173b164399b69b8
tommasocavalli08-beep <tommasocavalli.08@gmail.com>
2026-08-29 18:41:33 +0200

    Increase image delivery quality

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+const nextConfig = {
+  images: {
+    qualities: [95],
+    deviceSizes: [360, 390, 430, 640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [32, 48, 64, 96, 128, 256, 384]
+  }
+};
+
+export default nextConfig;
